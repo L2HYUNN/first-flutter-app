27f5c3b678908118cc794c1acc57ffc691c0ef5f
L2HYUNN <dhl9810@naver.com>
2022-05-30 01:15:31 +0900

    #22 More Dart Basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
+class Person {
+  String name = "MAX";
+  int age = 30;
+}
+
 double addNumbers(int num1, double num2) {
   // print(num1 + num2);
   return num1 + num2;
 }
 
 void main() {
-  print(addNumbers(1, 2));
+  var p1 = Person();
+  var p2 = Person();
+  print(p1.name);
+  p2.name = "MINUS";
+  print(p2.name);
+
+  // print(addNumbers(1, 2));
+
+  String name = "MAX";
+  int age = 30;
+  double firstResult;
+
+  // firstResult = addNumbers(1, 1);
+  // print(firstResult + 1);
+  // print("Hello~");
 }
