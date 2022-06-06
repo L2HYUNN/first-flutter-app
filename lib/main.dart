de6012cc9fa2bbedfd41496553deed3cef03f2be
L2HYUNN <dhl9810@naver.com>
2022-06-06 23:42:45 +0900

    #27 First Summary & Additional Syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,15 +12,5 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       home: Text("Hello!"),
     );
-  }
-}
-
-class Person {
-  String? name;
-  int? age;
-
-  Person(String name, int age) {
-    this.name = name;
-    this.age = age;
   }
 }
