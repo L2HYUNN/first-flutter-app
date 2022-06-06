78e87916b15b1acc73f507d81b2022803f538445
L2HYUNN <dhl9810@naver.com>
2022-06-06 19:23:47 +0900

    #24 Building an App From Scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,12 @@
-class Person {
-  String name = "MAX";
-  int age = 30;
-}
+import "package:flutter/material.dart";
 
-double addNumbers(int num1, double num2) {
-  // print(num1 + num2);
-  return num1 + num2;
-}
+void main() {}
 
-void main() {
-  var p1 = Person();
-  var p2 = Person();
-  print(p1.name);
-  p2.name = "MINUS";
-  print(p2.name);
-
-  // print(addNumbers(1, 2));
-
-  String name = "MAX";
-  int age = 30;
-  double firstResult;
-
-  // firstResult = addNumbers(1, 1);
-  // print(firstResult + 1);
-  // print("Hello~");
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text("Hello!"),
+    );
+  }
 }
